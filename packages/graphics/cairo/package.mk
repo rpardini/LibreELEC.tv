14eca93aa7ec0436bfccf27e831046ac51414251
Ricardo Pardini <ricardo@pardini.net>
2026-08-23 14:04:53 +0200

    cairo: use debian mirror, cairographics.org is 'nanas

--- packages/graphics/cairo/package.mk
+++ packages/graphics/cairo/package.mk
@@ -7,7 +7,7 @@ PKG_VERSION="1.18.4"
 PKG_SHA256="445ed8208a6e4823de1226a74ca319d3600e83f6369f99b14265006599c32ccb"
 PKG_LICENSE="LGPL-2.1-or-later OR MPL-1.1"
 PKG_SITE="https://cairographics.org/"
-PKG_URL="https://cairographics.org/releases/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_URL="http://deb.debian.org/debian/pool/main/c/cairo/cairo_${PKG_VERSION}.orig.tar.xz"
 PKG_DEPENDS_TARGET="toolchain zlib freetype fontconfig glib libpng pixman"
 PKG_LONGDESC="Cairo is a vector graphics library with cross-device output support."
 
