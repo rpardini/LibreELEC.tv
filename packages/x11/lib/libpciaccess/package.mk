1b69ca86d4e1f5dc7c4e1dcd8e97f71d14091431
Ricardo Pardini <ricardo@pardini.net>
2026-08-23 13:56:30 +0200

    libpciaccess: use slackware mirror, freedesktop is 'nanas

--- packages/x11/lib/libpciaccess/package.mk
+++ packages/x11/lib/libpciaccess/package.mk
@@ -7,7 +7,7 @@ PKG_VERSION="0.19"
 PKG_SHA256="3c55aa86c82e54a4e3109786f0463530d53b36b6d1cfd14616454f985dd2aa43"
 PKG_LICENSE="MIT"
 PKG_SITE="https://freedesktop.org"
-PKG_URL="https://xorg.freedesktop.org/archive/individual/lib/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_URL="https://mirrors.slackware.com/slackware/slackware-current/source/x/x11/src/lib/libpciaccess-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain util-macros zlib"
 PKG_LONGDESC="X.org libpciaccess library."
 
